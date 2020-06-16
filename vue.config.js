55751a0e9f177856878ddcb2f51b60c4c82906c0
FanSolomon <jetonlyy@gmail.com>
2020-06-16 18:50:21 +0800

    update configuration

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,3 @@
-// vue.config.js
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? './'
@@ -10,29 +9,8 @@ module.exports = {
       // 为开发环境修改配置...
     }
   },
-  css: {
-    extract: false
-  },
   // 它支持webPack-dev-server的所有选项
   devServer: {
-    proxy: {
-      // host: "localhost",
-      // port: 8080, // 端口号
-      // https: false, // https:{type:Boolean}
-      // open: false, // 配置自动启动浏览器
-      // proxy: 'http://localhost:4000' // 配置跨域处理,只有一个代理
-
-      // 配置多个代理
-      '/api': {
-        target: 'http://127.0.0.1:8080',
-        changeOrigin: true,
-        ws: false,
-        pathRewrite: {
-          '^/api': '/mock'
-        }
-
-      }
-    }
+    proxy: 'http://localhost:8080'
   }
-
 }
